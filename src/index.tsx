9b410351eef68439223dc7021ae9007a9d4ca795
Sami Kalmi <sami.kalmi@gmail.com>
2022-03-23 17:02:32 +0200

    Add SCSS styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import TimeTrial from "./timeTrial/TimeTrial";
