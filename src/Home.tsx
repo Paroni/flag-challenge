de60cb2fec68cd9e1f769837312d26bf76e069f2
Sami Kalmi <sami.kalmi@gmail.com>
2022-03-24 14:10:13 +0200

    Add Home layout and basic responsiveness

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,10 +4,15 @@ import NavigationButton from "./NavigationButton";
 function Home() {
   return (
     <div className="Home">
-      <NavigationButton target={"/challenge"} text={"Play Flag Challenge!"} />
-      <NavigationButton target={"/leaderboards"} text={"Leaderboards"} />
-      <NavigationButton target={"/about"} text={"About"} />
-      <NavigationButton target={"/author"} text={"Author"} />
+      <div className={"Home-information"}>
+        Welcome to Flag Challenge, an educational game for learning the flags of Europe!
+      </div>
+      <div className={"Home-navigation-container"}>
+        <NavigationButton target={"/challenge"} text={"Play Flag Challenge!"} />
+        <NavigationButton target={"/leaderboards"} text={"Leaderboards"} />
+        <NavigationButton target={"/about"} text={"About"} />
+        <NavigationButton target={"/author"} text={"Author"} />
+      </div>
     </div>
   );
 }
